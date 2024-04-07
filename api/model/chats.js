77c6f745114132422286019b75f7d5c3a5692301
JerinOfficial2001 <jerinjerijree@gmail.com>
2024-04-07 16:53:05 +0530

    WC_Chats

--- api/model/chats.js
+++ api/model/chats.js
@@ -1,16 +1,16 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 let Chats;
 
 if (mongoose.models && mongoose.models.Chats) {
-  Chats = mongoose.model('Chats');
+  Chats = mongoose.model("WC_Chats");
 } else {
   const ChatsSchema = new mongoose.Schema({
     sender: String,
     receiver: String,
   });
 
-  Chats = mongoose.model('Chats', ChatsSchema);
+  Chats = mongoose.model("WC_Chats", ChatsSchema);
 }
 
 export default Chats;
